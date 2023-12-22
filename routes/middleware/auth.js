bb97f0a26e3e56d49c1f7726fdad871c12591bd6
suraj <singhsurajraj1997@gmail.com>
2023-12-22 19:00:14 +0530

    code changes

--- routes/middleware/auth.js
+++ routes/middleware/auth.js
@@ -9,16 +9,16 @@ const fs = require('fs');
 env.config();
 
 
-// const storage = multer.diskStorage({
-//     destination: function(req,file,cb){
-//         cb(null,'./public/uploads')
-//     },
-//     filename: function(req,file,cb){
-//         cb(null,shortid.generate() + '-' + file.originalname);
-//     }
-// })
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'./public/uploads')
+    },
+    filename: function(req,file,cb){
+        cb(null,shortid.generate() + '-' + file.originalname);
+    }
+})
 
-const storage = multer.memoryStorage();
+// const storage = multer.memoryStorage();
 
 
 // const s3 = new aws.S3({
